4f1e24f51222e586acaa0b9dd79b9ea12f3833da
Miles <sino_miles@foxmail.com>
2019-05-06 16:06:30 +0800

    添加项目文件。

---
+++ BooksApi/Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BooksApi.Models;
+using BooksApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookService;
+        
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+        [HttpGet]
+        public async Task<List<Book>> Get()
+        {
+            return await _bookService.Get();
+        }
+        [HttpPost]
+        public async Task<Book> Create(Book book)
+        {
+            return await _bookService.Create(book);
+        }
+    }
+}
---
+++ BooksApi/Services/BookService.cs
@@ -0,0 +1,46 @@
+﻿using BooksApi.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApi.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+        public BookService(IConfiguration configuration)
+        {
+            //获取操作实例 进行构造注入
+            var client = new MongoClient(configuration.GetConnectionString("MongoDBConnString"));
+            var database = client.GetDatabase("DB");
+            _books = database.GetCollection<Book>("Books");
+        }
+        public async Task<Book> Create(Book book)
+        {
+            await _books.InsertOneAsync(book);
+            return book;
+        }
+        public async Task<List<Book>> Get()
+        {
+           var result= await _books.FindAsync(book=> true);
+           return result.ToList();
+        }
+        public async Task Update(string id, Book bookIn)
+        {
+          await  _books.ReplaceOneAsync(book => book.Id == id, bookIn);
+        }
+
+        public async Task Remove(Book bookIn)
+        {
+           await _books.DeleteOneAsync(book => book.Id == bookIn.Id);
+        }
+
+        public async Task Remove(string id)
+        {
+           await _books.DeleteOneAsync(book => book.Id == id);
+        }
+    }
+}
